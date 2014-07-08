d4b6d6d14190169be8276e60ab9682476034674b
matrix41 <raymond@vivaldi.ipac.caltech.edu>
2014-07-08 13:39:58 -0700

    identified all the column fields (ie column numbers) that I will need to make planet parameter files

--- ingest.pl
+++ ingest.pl
@@ -25,8 +25,41 @@ close ($fh);
 # print "$array[37]\n"; # this will print all the metric units (m/s, deg, AU, ....)
 
 
+my @split_entry;
+# Line 38 is the beginning of the data 
+for ( my $j = 38; $j <= $#array; $j++ )
+{
+#  print $array[$j];
+  @split_entry = split( /,/, $array[$j] );
+#  print "$split_entry[0]  $split_entry[1]  $split_entry[2]  $split_entry[3]\n";
+  # print "\nset heading off\n";
+  # print "select OBJECTID, ALIASDIS\n";
+  # print "from EXOP_LIT_ALIAS_TBL\n";
+  # print "where EXOP_LIT_ALIAS_TBL.ALIASDIS like '%$split_entry[1] $split_entry[2]%';\n\n";
+
+# $split_entry[1] : 47 UMa
+# $split_entry[2] : c
+# $split_entry[4] : plnorbper
+# $split_entry[5] : plnorbpererr1 / plnorbpererr2 
+# $split_entry[7] : plnrvamp 
+# $split_entry[8] : plnrvamperr1 / plnrvamperr2 
+# $split_entry[10] : plnorbeccen 
+# $split_entry[11] : plnorbeccenerr1 / plnorbeccenerr2 
+# $split_entry[13] : plnorblper 
+# $split_entry[14] : plnorblpererr1 / plnorblpererr2 
+# $split_entry[16] : plnorbtper 
+# $split_entry[18] : plnorbtpererr1 / plnorbtpererr2 
+# $split_entry[24] : plnmsinij 
+# $split_entry[25] : plnmsinijerr1 / plnmsinijerr2 
+# $split_entry[27] : plnorbsmax 
+# $split_entry[28] : plnorbsmaxerr1 / plnorbsmaxerr2 
+  print "$split_entry[1] $split_entry[2] $split_entry[4] $split_entry[5] $split_entry[7] $split_entry[8] $split_entry[10] $split_entry[11] $split_entry[13] $split_entry[14] $split_entry[16] $split_entry[18] $split_entry[24]$split_entry[25] $split_entry[27] $split_entry[28]\n"; 
+}
+
+
+
 # Create hash keys from the column headers 
-my @splitarray = split(/,/, $array[36]);
+my @splitarray = split(/,/, $array[36]); # Line 36 are the column headers 
 
 # OLD
 # my %butler_hash = ();
