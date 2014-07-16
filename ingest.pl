de8e1cc150089b65ff1ded50894402e367fef4a6
matrix41 <raymond@vivaldi.ipac.caltech.edu>
2014-07-16 16:37:40 -0700

    declared the three variables outside the FOR loop instead of inside the FOR loop

--- ingest.pl
+++ ingest.pl
@@ -57,6 +57,9 @@ close ($fh);
 open ( my $fh2, '>', $outputfile ) or die "\nCould not open file $outputfile $!\n";
 
 my %outbound_hash = ();
+my $howmanyA;
+my $howmanyB;
+my $sigdig;
 
 my @split_entry;
 # Line 38 is the beginning of the data 
@@ -131,14 +134,14 @@ for ( my $j = 38; $j <= $#array; $j++ )
 #      $outbound_hash{'plnorbtpererr2'} = -$split_entry[18];
 
 # Crazy Step 1 of 4: Calculate the length of decimal number. 
-      my $howmanyA = length($split_entry[18]);
+      $howmanyA = length($split_entry[18]);
 #      print "howmanyA = $howmanyA\n\n";
 # Crazy Step 2 of 4: Calculate the integer portion of decimal number. 
-      my $howmanyB = length(int($split_entry[18]));
+      $howmanyB = length(int($split_entry[18]));
 #      print "howmanyB = $howmanyB\n\n";
 # Crazy Step 3 of 4: Now subtract the length of integer portion from the original length.
 #                    Also, subtract one more to account for the decimal place. 
-      my $sigdig = $howmanyA-$howmanyB-1;
+      $sigdig = $howmanyA-$howmanyB-1;
 #      print "sigdig = $sigdig\n\n";
 # Crazy Step 4 of 4: Now use the computed length $sigdig to control the 
 #                    number of significant digits. 
